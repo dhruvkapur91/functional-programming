9971cf2dcf4301230d56f254117789b7fc6bac28
Dhruv Kapur <dhruvk@thoughtworks.com>
2018-01-04 21:17:26 +0530

    Add reset mocks for testing purpose

--- src/test/scala/CoffeeShopSpec.scala
+++ src/test/scala/CoffeeShopSpec.scala
@@ -1,6 +1,6 @@
 import org.scalatest.{FunSuite, Matchers}
 
-class CoffeeShopSpec extends FunSuite with Matchers {
+class CoffeeShopSpec extends FunSuite with Matchers with ResetMocks {
 
   test("should charge total of $11 ($10 + $1 fee) when buying a cup of coffee") {
     CoffeeShop.buy(MockedCreditCard) should be(Coffee())
--- src/test/scala/MockedCreditCard.scala
+++ src/test/scala/MockedCreditCard.scala
@@ -2,7 +2,13 @@ import SideEffect.CreditCard
 
 case object MockedCreditCard extends CreditCard("Mocked") {
   var chargedAmount : Int = -1
+  var calledNumberOfTimes : Int = 0
   override def charge(price: Int) = {
     chargedAmount = price + 1
+    calledNumberOfTimes += 1
+  }
+  def reset() = {
+    chargedAmount = -1
+    calledNumberOfTimes = 0
   }
 }
