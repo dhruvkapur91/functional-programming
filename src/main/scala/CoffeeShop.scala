61d8de112bab59ba92187fb4808000cc4b5c326d
Dhruv Kapur <dhruvk@thoughtworks.com>
2018-01-13 17:15:47 +0530

    Functional buying many

--- src/main/scala/CoffeeShop.scala
+++ src/main/scala/CoffeeShop.scala
@@ -12,15 +12,24 @@ case class Coffee() {
   val price = 10
 }
 
-case class Charge(price: Int)
+case class Charge(price: Int) {
+  def +(other : Charge) = Charge(price + other.price)
+}
 
-case class Order(coffee: Coffee, charge: Charge)
+case class Order(coffees: Seq[Coffee], charge: Charge) {
+  def +(other : Order) = Order(coffees ++ other.coffees, charge + other.charge)
+}
 
 object CoffeeShop {
+  def buyMany(creditCard: CreditCard, numberOfCups: Int) : Order = {
+    val orders = (1 to numberOfCups).map(_ => buy(creditCard))
+    orders.reduce((one,two) => one + two)
+  }
+
 
   def buy(creditCard: CreditCard): Order = {
     val coffee = Coffee()
-    Order(coffee, Charge(coffee.price))
+    Order(Seq(coffee), Charge(coffee.price))
   }
 
 }
