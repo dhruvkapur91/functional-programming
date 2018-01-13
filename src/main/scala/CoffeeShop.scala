732d00f90d4653e71a4abef25b2161bbe53775f1
Dhruv Kapur <dhruvk@thoughtworks.com>
2018-01-13 16:38:22 +0530

    Functional buying one cup of coffee

--- src/main/scala/CoffeeShop.scala
+++ src/main/scala/CoffeeShop.scala
@@ -1,21 +1,26 @@
 import SideEffect.CreditCard
 
 object SideEffect {
-  class CreditCard(number : String) {
-    def charge(price : Int): Unit = ???
+
+  class CreditCard(number: String) {
+    def charge(price: Int): Unit = ???
   }
+
 }
+
+case class Charge(price: Int, creditCard: CreditCard)
+
+case class Order(items: Coffee, charge: Charge)
 
 case class Coffee() {
   val price = 10
 }
 
-
 object CoffeeShop {
 
-  def buy(creditCard: CreditCard): Coffee = {
+  def buy(creditCard: CreditCard): Order = {
     val coffee = Coffee()
-    creditCard.charge(coffee.price)
-    coffee
+    //    creditCard.charge(coffee.price)
+    Order(coffee, Charge(coffee.price, creditCard))
   }
 }
--- src/test/scala/CoffeeShopSpec.scala
+++ src/test/scala/CoffeeShopSpec.scala
@@ -1,10 +1,14 @@
-import CustomMatchers._
 import org.scalatest.{FunSuite, Matchers}
 
 class CoffeeShopSpec extends FunSuite with Matchers {
 
-  test("should call credit card with total of $10 when buying a cup of coffee") {
-    CoffeeShop.buy(MockedCreditCard) should be(Coffee())
-    MockedCreditCard should be(calledWith(10))
+//  test("should call credit card with total of $10 when buying a cup of coffee") {
+//    CoffeeShop.buy(MockedCreditCard) should be(Coffee())
+//    MockedCreditCard should be(calledWith(10))
+//  }
+
+  test("buying a cup of coffee should return an order having a cup of coffee with charge of $10") {
+    val coffee = Coffee()
+    CoffeeShop.buy(MockedCreditCard) should be(Order(coffee, Charge(10, MockedCreditCard)))
   }
 }
