0f294bb11118f1890f34bc7607c36e3970384e86
Priyam Mitra <pmitra43@gmail.com>
2018-01-14 14:37:50 +0530

    FP. Integration buy many

--- src/main/scala/CoffeeShopCounter.scala
+++ src/main/scala/CoffeeShopCounter.scala
@@ -1,8 +1,8 @@
 import SideEffect.CreditCard
 
 object CoffeeShopCounter {
-  def buy(creditCard: CreditCard) : Unit = {
-    val order = CoffeeShop.buy()
+  def buy(creditCard: CreditCard, numberOfCups : Int = 0) : Unit = {
+    val order = CoffeeShop.buyMany(numberOfCups)
     creditCard.charge(order.price)
     println(order)
   }
--- src/test/scala/CoffeeShopCounterSpec.scala
+++ src/test/scala/CoffeeShopCounterSpec.scala
@@ -2,9 +2,9 @@ import org.scalatest.{FunSuite, Matchers}
 
 class CoffeeShopCounterSpec extends FunSuite with Matchers  {
 
-  test("should call credit card with 10$ when buying a cup of coffee") {
-    CoffeeShopCounter.buy(MockedCreditCard)
-    MockedCreditCard.calledWith should be(10)
+  test("should call credit card with 20$ when buying two cups of coffee") {
+    CoffeeShopCounter.buy(MockedCreditCard, numberOfCups = 2)
+    MockedCreditCard.calledWith should be(20)
   }
 
 }
