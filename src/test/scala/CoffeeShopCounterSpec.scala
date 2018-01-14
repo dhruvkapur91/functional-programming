9874cc889072014fce18d12010dc8707e8cb32c4
Priyam Mitra <pmitra43@gmail.com>
2018-01-14 13:11:09 +0530

    integration test to buy many coffees revisited

--- src/test/scala/CoffeeShopCounterSpec.scala
+++ src/test/scala/CoffeeShopCounterSpec.scala
@@ -1,7 +1,7 @@
 import org.scalatest.{FunSuite, Matchers}
 
 class CoffeeShopCounterSpec extends FunSuite with Matchers {
-  test("should charge total $20 when buying two cups of coffee") {
+  test("should call credit card with total of $20 when buying two cups of coffee") {
     CoffeeShopCounter.buy(MockedCreditCard, numberOfCups = 2)
     MockedCreditCard.calledWith should be(20)
   }
